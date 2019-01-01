14a6eeaf5ce11e5198b843f46197e58cd8f7175c
amyard <maksymturenko@gmail.com>
2019-01-01 23:44:42 +0200

    almost finished with models

--- MovieMDB/core/admin.py
+++ MovieMDB/core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from core.models import Movie
+from core.models import Movie, Person, Role
 
 
 # Register your models here.
 
 admin.site.register(Movie)
+admin.site.register(Person)
+admin.site.register(Role)
