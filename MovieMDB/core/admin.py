30d7efb3325fe596f8ffab3f0e652b2b9b550993
amyard <maksymturenko@gmailcom>
2019-01-13 22:18:34 +0300

    'delete'

--- MovieMDB/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from core.models import Movie, Person, Role
-
-
-# Register your models here.
-
-admin.site.register(Movie)
-admin.site.register(Person)
-admin.site.register(Role)
