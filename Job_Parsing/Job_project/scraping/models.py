8e9382baf1ee4ec759097f68d833ae703d0e4aec
amyard <maksymturenko@gmailcom>
2019-01-23 12:03:08 +0300

    'del'

--- Job_Parsing/Job_project/scraping/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class City(models.Model):
-	name = models.CharField(max_length = 50, verbose_name = 'Город', unique = True)
-
-	def __str__(self):
-		return self.name
