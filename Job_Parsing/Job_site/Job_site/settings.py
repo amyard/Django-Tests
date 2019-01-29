86704d4c2601ae5133d618ca6bd00a6c73841189
amyard <maksymturenko@gmailcom>
2019-01-29 17:34:39 +0300

    'settings'

--- Job_Parsing/Job_site/Job_site/settings.py
+++ Job_Parsing/Job_site/Job_site/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Job_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Job_Parsing/Job_site/core/views.py
+++ Job_Parsing/Job_site/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
 
 # Create your views here.
+def home(requests):
+	return HttpResponse('<h1>Scorpion Wins!!!</h1>')
