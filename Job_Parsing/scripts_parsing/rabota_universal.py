86a1b387bc0b1c3a5675a28a7dfd9910830e0199
amyard <maksymturenko@gmailcom>
2019-01-29 14:38:04 +0300

    'main_script'

--- Job_Parsing/scripts_parsing/rabota_universal.py
+++ Job_Parsing/scripts_parsing/rabota_universal.py
@@ -10,12 +10,47 @@ import re
 import time
 
 
-if len(sys.argv)>3:
-	city = sys.argv[-1].lower()
-	job = ' '.join([ argv.lower() for argv in sys.argv[1:-1] ])
-else:
-	city = sys.argv[-1].lower()
-	job = sys.argv[1].lower()
+def get_main_info():
+	if len(sys.argv)>3:
+		translate = {'все регионы': 0, 
+					 'киев': 1,
+					 'львов': 2,
+					 'одесса': 3, 
+					 'днепр': 4,
+					 'винница': 5,
+					 'донецк': 6,
+					 'житомир': 7,
+					 'запорожье': 9,
+					 'ивано-франковск': 10,
+					 'кропивницкий': 11,
+					 'луганск': 13,				 
+					 'луцк': 14,
+					 'николаев': 15,
+					 'полтава': 17,
+					 'ровно': 18,
+					 'сумы': 19,
+					 'тернополь': 20,
+					 'харьков': 21,
+					 'херсон': 22,
+					 'хмельницкий': 23,
+					 'черкассы': 24,
+					 'чернигов': 25,
+					 'черновцы': 26,				 
+					 'мариуполь': 27,
+					 'ужгород': 28,
+					 'симферополь': 29,
+					 'кривой рог': 31,
+					 'севастополь': 32}
+		city = translate.get(sys.argv[-1].lower(),sys.argv[-1].lower())
+		job = '+'.join([ argv.lower() for argv in sys.argv[1:-1] ])
+		base_url = f'https://rabota.ua/jobsearch/vacancy_list?regionId={city}&keyWords={job}'
+		pages = base_url+'&pg='
+	else:
+		city = sys.argv[-1].lower()
+		job = sys.argv[1].lower()
+		base_url = f'https://rabota.ua/zapros/{job}/{city}'
+		pages = base_url+'/pg'
+	return city, job, base_url, pages
 
 
 ua = UserAgent()
@@ -27,21 +62,6 @@ urls = []
 jobs = []
 
 
-def get_url(url):
-	options = webdriver.ChromeOptions()
-	# options.add_argument('--headless')
-	# driver = webdriver.Chrome(chrome_options=options)
-	driver = webdriver.Chrome()
-	driver.get(url)
-	driver.find_element(By.XPATH, '//input[@id="ctl00_content_vacSearch_Keyword"]').send_keys(job)
-	driver.find_element(By.XPATH, '//input[@id="ctl00_content_vacSearch_CityPickerWork_inpCity"]').clear()
-	driver.find_element(By.XPATH, '//input[@id="ctl00_content_vacSearch_CityPickerWork_inpCity"]').send_keys(city)
-	time.sleep(1)
-	driver.find_element(By.XPATH, '//a[@id="ctl00_content_vacSearch_lnkSearch"]').click()
-	url = driver.current_url
-	driver.quit()
-	return url
-
 
 def get_html(url):
     req = session.get(url, headers=headers)
@@ -52,6 +72,7 @@ def get_html(url):
 def get_total_vacations(html):
 	soup = BeautifulSoup(html, 'html.parser')
 	total = soup.find('span', class_= 'fd-fat-merchant').text
+	print(f'We have {int(np.ceil(int(total)/20))} pages ({int(total)}  vacancies).')
 	return(np.ceil(int(total)/20))
 
 
@@ -72,29 +93,27 @@ def get_data(html, domain):
 
 
 def main():
+
 	domain = 'https://rabota.ua'
 
-	# получил url нашей страници
-	base_url = get_url(domain)
+	# получим значения
+	city, job, base_url, pages = get_main_info()
 
 	# проверяем правильность запроса
 	req = session.get(base_url, headers=headers)
 	if req.status_code == 200:
-		pages = base_url+'&pg=' if re.findall('vacancy_list', base_url) else base_url+'/pg'
 		total_pages = get_total_vacations(get_html(base_url))
-		print(f'We have {int(total_pages)} pages.')
-
 
 		# парсим
 		for i in range(1, int(total_pages)+1):
-			print('Loading {}%'.format((int(i)/int(total_pages))*100))
+			print('Loading {}%'.format(round((int(i)/int(total_pages))*100)))
 			url = pages+str(i)
+			print(url)
 			get_data(get_html(url), domain)
 
 	elif req.status_code != 200:
 		print('Wrong data')
 	
-
 
 
 if __name__ == '__main__':
